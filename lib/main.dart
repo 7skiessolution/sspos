3c0a4355d1a4260ac29ce911f14bf36dc2be3bcd
MohsinIsmail28 <mohsinkhan876k@gmail.com>
2024-03-23 15:37:23 +0500

    mainchange

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
    if (kIsWeb) {
     await Firebase.initializeApp(
       options: const FirebaseOptions(
